577f3808a5a1b54c2a0d8ab2889146f9004599ed
Ujjwal807 <ambikastore.uk@gmail.com>
2024-12-08 09:54:52 +0530

    navupd

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
+        <Navbar/>
         {children}
       </body>
     </html>
