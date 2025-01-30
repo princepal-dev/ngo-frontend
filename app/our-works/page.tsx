d0671c3f92e6e744652376dd2c56da86d1c043bb
Ujjwal807 <ambikastore.uk@gmail.com>
2025-01-30 20:14:19 +0530

    update

--- app/our-works/page.tsx
+++ app/our-works/page.tsx
@@ -2,16 +2,16 @@ import React from "react";
 import Image from "next/image";
 import type { Metadata } from "next";
 import fund from "@/public/donation-img.png";
-import img1 from "@/public/gallery/g1.jpg"
-import img2 from "@/public/gallery/g2.jpg"
-import img3 from "@/public/gallery/g3.jpg"
-import img4 from "@/public/gallery/g4.jpg"
-import img5 from "@/public/gallery/g5.jpg"
-import img6 from "@/public/gallery/g6.jpg"
-import img7 from "@/public/gallery/g7.jpg"
-import img8 from "@/public/gallery/g8.jpg"
-import img9 from "@/public/gallery/g9.jpg"
-import img10 from "@/public/gallery/g10.jpg"
+import img1 from "@/public/gallery/g1.jpg";
+import img2 from "@/public/gallery/g2.jpg";
+import img3 from "@/public/gallery/g3.jpg";
+import img4 from "@/public/gallery/g4.jpg";
+import img5 from "@/public/gallery/g5.jpg";
+import img6 from "@/public/gallery/g6.jpg";
+import img7 from "@/public/gallery/g7.jpg";
+import img8 from "@/public/gallery/g8.jpg";
+import img9 from "@/public/gallery/g9.jpg";
+import img10 from "@/public/gallery/g10.jpg";
 import { Button } from "@/components/ui/button";
 import {
   Carousel,
@@ -52,7 +52,6 @@ export default async function OurWorkPage() {
           <Image src={fund} alt="fundraiser-img" />
         </div>
       </section>
-
       <section className="my-10">
         <h2 className="text-center text-3xl mb-8 font-bold text-green-600">
           Mid-Day Meals for Anganwadi
@@ -91,8 +90,13 @@ export default async function OurWorkPage() {
             </CarouselItem>
           </CarouselContent>
           <CarouselPrevious className="hidden lg:block" />
-          <CarouselNext className="hidden lg:block"/>
+          <CarouselNext className="hidden lg:block" />
         </Carousel>
+      </section>
+      <section className="my-10">
+        <h2 className="text-center text-3xl mb-8 font-bold text-green-600">
+          Mid-Day Meals for Anganwadi
+        </h2>
         <Carousel className="px-3 my-4">
           <CarouselContent>
             <CarouselItem className="md:basis-1/2 lg:basis-1/3">
@@ -127,8 +131,14 @@ export default async function OurWorkPage() {
             </CarouselItem>
           </CarouselContent>
           <CarouselPrevious className="hidden lg:block" />
-          <CarouselNext className="hidden lg:block"/>
-        </Carousel> <Carousel className="px-3 my-4">
+          <CarouselNext className="hidden lg:block" />
+        </Carousel>
+      </section>{" "}
+      <section className="my-10">
+        <h2 className="text-center text-3xl mb-8 font-bold text-green-600">
+          Mid-Day Meals for Anganwadi
+        </h2>
+        <Carousel className="px-3 my-4">
           <CarouselContent>
             <CarouselItem className="md:basis-1/2 lg:basis-1/3">
               <Image src={img1} quality={80} alt="img-1" />
@@ -162,7 +172,7 @@ export default async function OurWorkPage() {
             </CarouselItem>
           </CarouselContent>
           <CarouselPrevious className="hidden lg:block" />
-          <CarouselNext className="hidden lg:block"/>
+          <CarouselNext className="hidden lg:block" />
         </Carousel>
       </section>
     </main>
